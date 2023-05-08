d95dba8b83b6dd08a89dc92b04b72e673453d8d0
nhhasib <nafiul.hasan1997@gmail.com>
2023-05-09 01:27:07 +0600

    news/id and catagory/:id added

--- index.js
+++ index.js
@@ -21,6 +21,22 @@ app.get('/news', (req, res) => {
     res.send(news)
 })
 
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const newsSelected = news.find(n => n._id === id)
+    res.send(newsSelected)
+})
+app.get('/catagories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const catagoriesNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(catagoriesNews)
+    }
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
